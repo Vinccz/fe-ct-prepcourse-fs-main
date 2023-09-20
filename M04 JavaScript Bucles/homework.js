8afe9c68f17e52a135ac1be80c11e6f756780337
Martin Daniel Bevilacqua <131474810+Vinccz@users.noreply.github.com>
2023-09-20 08:16:51 -0300

    rehaciendo Exercices

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -167,18 +167,34 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+         return false;
+      }
+   }
+   return true;
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   if (valor) {
+      return "Soy verdadero";
+   } else {
+      return "Soy falso";
+   }
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   if (num.toString().length == 3) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function doWhile(num) {
@@ -186,6 +202,13 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   let i = 0;
+   let valor = num;
+   do {
+      valor += 5;
+      i++;
+   } while (i < 8);
+   return valor;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
