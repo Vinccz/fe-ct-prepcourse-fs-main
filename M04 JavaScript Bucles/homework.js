ea8ca775cd5b00218c8149e21831f0ca242dc6e0
Vinccz <martinbv@live.com.ar>
2023-09-28 14:29:06 -0300

    Checkpoint

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -102,9 +102,9 @@ function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
    // De lo contrario, retornar false.
    // Tu código:
-   if (num >= 20 && num <= 50) {
+   if (num < 50 && num > 20) {
       return true;
-   } else {
+   }else {
       return false;
    }
 }
@@ -148,17 +148,17 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-   if (num1 > num2 && num1 > num3 && num >= 0) {
-      return "Numero 1 es mayor y positivo";
-   } else if ( num1 < 0 || num2 < 0 || num3 < 0){
-      return "Hay negativos";
-   } else if (num3 > num1 && num3 < num2) {}
-      return num3++;   
-   } if ( num1 === 0 && num2 === 0 && num3 === 0) {
-   return "Error";
-   } else {
-   return false;
+      if (num3 > num1 && num3 > num2) {
+         return (num3+1);
+      } else if (num1 === 0 && num2 === 0 && num3 === 0) {
+         return "Error";
+      } if (num1 < 0 || num2 < 0 || num3 < 0) {
+         return "Hay negativos";
+      } if (num1 > num2 && num1 > num3 && num1 >= 0) {
+         return "Numero 1 es mayor y positivo";
+      } else {return false;}
    }
+   
 
 function esPrimo(num) {
    // Retornar true si "num" es primo.
